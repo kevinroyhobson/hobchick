3266da4d2b1ef2eb78428ecc5c9062fefd329154
Kevin Hobson <kevinroyhobson@gmail.com>
2014-11-11 23:26:36 -0500

    Plane animations to Pittsburgh

--- hobchick/hobchick/static/js/where.js
+++ hobchick/hobchick/static/js/where.js
@@ -1,6 +1,32 @@
 
 var _fallingWaterLocation = L.latLng(39.906560, -79.467913);
 var _lgaLocation = L.latLng(40.77591055, -73.872799873);
+var _cvgLocation = L.latLng(39.055584, -84.661873);
+var _cmhLocation = L.latLng(40.000597, -82.887260);
+var _pitLocation = L.latLng(40.496179, -80.241419);
+
+var newYorkPlane = {
+    "startingLocation": _lgaLocation,
+    "numTotalSteps": 400,
+    "latitudeJitterCoefficient": 30000
+};
+
+var cincinnatiPlane = {
+    "startingLocation": _cvgLocation,
+    "numTotalSteps": 400,
+    "latitudeJitterCoefficient": -20000
+};
+
+var columbusPlane = {
+    "startingLocation": _cmhLocation,
+    "numTotalSteps": 400,
+    "latitudeJitterCoefficient": 50000
+}
+
+var _planes = [ newYorkPlane, cincinnatiPlane, columbusPlane ];
+var _maxPlaneSteps = 1000;
+
+var _map;
 
 $(function() {
     initializeMap();
@@ -20,30 +46,59 @@ function onResize() {
 function initializeMap() {
 
     L.mapbox.accessToken = 'pk.eyJ1Ijoia2V2aW5yb3lob2Jzb24iLCJhIjoiRmpkMTJoNCJ9.Ki1Wowu9Y_CvTp0cQk4iBg';
-    var map = L.mapbox.map('mapbox-map', 'kevinroyhobson.k618jbcp')
+    _map = L.mapbox.map('mapbox-map', 'kevinroyhobson.k618jbcp')
       .setView(_fallingWaterLocation, 7);
 
-    var marker = L.marker(_lgaLocation, {
+    _.each(_planes, function(plane) {
+        initializePlane(plane);
+    });
+
+    movePlanesRecursively();
+}
+
+function initializePlane(plane) {
+    plane.marker = L.marker(plane.startingLocation, {
         icon: L.mapbox.marker.icon({
           iconUrl: 'https://www.mapbox.com/maki/renders/airport-24@2x.png',
           iconSize: [24, 24],
         })
     });
-
-    var t = 0;
-    window.setInterval(function() {
-
-        var nycPlaneCoordinates = marker.getLatLng();
-
-        var overallDistance = _lgaLocation.distanceTo(_fallingWaterLocation);
-
-        
+    plane.marker.addTo(_map);
+}
 
 
-    }, 50);
+var _numCurrentPlaneSteps = 0;
 
-    marker.addTo(map);
+function movePlanesRecursively() {
 
+    _.each(_planes, function(plane) {
+        if (_numCurrentPlaneSteps < plane.numTotalSteps) {
+            movePlane(plane);
+        }
+    });
+
+    _numCurrentPlaneSteps++;
+
+    if (_numCurrentPlaneSteps < _maxPlaneSteps) {
+        setTimeout(function() {
+            movePlanesRecursively();
+        }, 10);
+    }
+}
+
+function movePlane(plane) {
+    var newLatLng = computeNewLatLngForPlane(plane);
+    plane.marker.setLatLng(newLatLng);
+}
+
+function computeNewLatLngForPlane(plane) {
+    var latitudeStep = (_pitLocation.lat - plane.startingLocation.lat) / plane.numTotalSteps;
+    var longitudeStep = (_pitLocation.lng - plane.startingLocation.lng) / plane.numTotalSteps;
+
+    var latitudeJitterForCurve = ((plane.numTotalSteps / 2.0) - _numCurrentPlaneSteps) / plane.latitudeJitterCoefficient;
+
+    return L.latLng(plane.marker.getLatLng().lat + latitudeStep + latitudeJitterForCurve,
+                    plane.marker.getLatLng().lng + longitudeStep);
 }
 
 
