0d5f6aaeb76740293dde52a684372e861c042f60
Kevin Hobson <kevinroyhobson@gmail.com>
2014-12-30 12:02:53 -0500

    added markers for our points of interest in PA

--- hobchick/hobchick/static/js/where.js
+++ hobchick/hobchick/static/js/where.js
@@ -1,5 +1,8 @@
 
-var _fallingWaterLocation = L.latLng(39.906560, -79.467913);
+var _fallingwaterLocation = L.latLng(39.906230, -79.467922);
+var _barnLocation = L.latLng(39.906607, -79.460282);
+var _sevenSpringsLocation = L.latLng(40.023137, -79.297773);
+
 var _lgaLocation = L.latLng(40.77591055, -73.872799873);
 var _cvgLocation = L.latLng(39.055584, -84.661873);
 var _cmhLocation = L.latLng(40.000597, -82.887260);
@@ -10,6 +13,9 @@ var _ordLocation = L.latLng(41.974455, -87.907194);
 var _iadLocation = L.latLng(38.851540, -77.040170);
 
 var _pitLocation = L.latLng(40.496179, -80.241419);
+
+var _centerOfMap = L.latLng((_fallingwaterLocation.lat + _sevenSpringsLocation.lat) / 2,
+                            (_fallingwaterLocation.lng + _sevenSpringsLocation.lng) / 2);
 
 var newYorkPlane = {
     "startingLocation": _lgaLocation,
@@ -67,7 +73,9 @@ var _secondsToWaitBeforePlanesMove = 0.75;
 var _startDisappearingAtStepNumber = 450;
 var _planesAreVisible = true;
 
-var _secondsUntilZoom = 5.5;
+var _stepsUntilZoom = 460;
+
+var _stepsToPopMarkers = 499;
 
 var _map;
 
@@ -89,8 +97,12 @@ function onResize() {
 function initializeMap() {
 
     L.mapbox.accessToken = 'pk.eyJ1Ijoia2V2aW5yb3lob2Jzb24iLCJhIjoiRmpkMTJoNCJ9.Ki1Wowu9Y_CvTp0cQk4iBg';
+
     _map = L.mapbox.map('mapbox-map', 'kevinroyhobson.k618jbcp')
-      .setView(_fallingWaterLocation, 7);
+      .setView(_centerOfMap, 7);
+
+    var startingBounds = L.latLngBounds([_cvgLocation, _lgaLocation]);
+    //_map.fitBounds(startingBounds);
 
     _.each(_planes, function(plane) {
         initializePlane(plane);
@@ -100,9 +112,6 @@ function initializeMap() {
         movePlanesRecursively();
     }, _secondsToWaitBeforePlanesMove * 1000);
 
-    setTimeout(function() {
-        centerAndZoom();
-    }, _secondsUntilZoom * 1000);
 }
 
 function initializePlane(plane) {
@@ -141,6 +150,15 @@ function movePlanesRecursively() {
     if (_numCurrentPlaneSteps >= _startDisappearingAtStepNumber && _planesAreVisible) {
         fadePlanes();
     }
+
+    if (_numCurrentPlaneSteps == _stepsUntilZoom) {
+        centerAndZoom();
+        //fitBoundsToPointsOfInterest();
+    }
+
+    if (_numCurrentPlaneSteps == _stepsToPopMarkers) {
+        addFallingwaterPointsOfInterest();
+    }
 }
 
 function movePlane(plane) {
@@ -152,8 +170,8 @@ function movePlane(plane) {
 }
 
 function computeNewLatLngForPlane(plane) {
-    var latitudeStep = (_fallingWaterLocation.lat - plane.startingLocation.lat) / _totalPlaneSteps;
-    var longitudeStep = (_fallingWaterLocation.lng - plane.startingLocation.lng) / _totalPlaneSteps;
+    var latitudeStep = (_fallingwaterLocation.lat - plane.startingLocation.lat) / _totalPlaneSteps;
+    var longitudeStep = (_fallingwaterLocation.lng - plane.startingLocation.lng) / _totalPlaneSteps;
 
     var latitudeJitterForCurve = ((_totalPlaneSteps / 2.0) - _numCurrentPlaneSteps) / plane.latitudeJitterCoefficient;
 
@@ -171,8 +189,6 @@ function computeNewAngleForPlane(plane, newLatLng) {
 
     // Correction for the stupid way the rotated marker works.
     angleInDegrees = 450 - angleInDegrees;
-
-    //console.log('xDelta = ' + xDelta + ', yDelta = ' + yDelta + ', angle = ' + angleInDegrees);
 
     return angleInDegrees;
 }
@@ -192,6 +208,7 @@ function centerAndZoom() {
     var shouldKeepZooming = true;
     var newZoomLevel = _map.getZoom() + 1;
     _map.setZoom(newZoomLevel);
+    _map.panTo(_centerOfMap);
 
     if (newZoomLevel >= 12) {
         shouldKeepZooming = false;
@@ -202,4 +219,81 @@ function centerAndZoom() {
             centerAndZoom();
         }, 15);
     }
+    else {
+        fitBoundsToPointsOfInterest();
+    }
+}
+
+function addFallingwaterPointsOfInterest() {
+
+    L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [
+                _fallingwaterLocation.lng,
+                _fallingwaterLocation.lat
+            ]
+        },
+        properties: {
+            title: 'Fallingwater',
+            description: "Designed in 1935, Frank Lloyd Wright's masterpiece was designated a National Historical Landmark in 1966.<br/><br/><a target='_blank' href='http://www.fallingwater.org/'>Information</a>&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://maps.google.com/maps?um=1&ie=UTF-8&fb=1&gl=us&geocode=KTHneU33MzWIMbiaCnnTKxZu&daddr=Fallingwater,%201491%20Mill%20Run%20Road,%20Mill%20Run,%20Pennsylvania%2015464&sa=X&ei=useiVL78E8WZgwTl44DoAw&ved=0CJcBEPUXMA8&output=classic&dg=brw'>Directions</a>",
+            'marker-size': 'medium',
+            'marker-color': '#222222',
+            'marker-symbol': 'star'
+        }
+    })
+    .addTo(_map);
+
+    L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [
+                _barnLocation.lng,
+                _barnLocation.lat
+            ]
+        },
+        properties: {
+            title: 'The Barn at Fallingwater',
+            description: "Built in 1870, the Barn at Fallingwater was entrusted to the Western Pennsylvania Conservancy in 1963.<br/><br/><a target='_blank' href='http://www.fallingwater.org/49/about-the-barn-at-fallingwater'>Information</a>&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://maps.google.com/maps?um=1&ie=UTF-8&fb=1&gl=us&geocode=KROB5TsGNDWIMXuGpBdfLPl-&daddr=The%20Barn%20at%20Fallingwater,%20Mill%20Run%20Road,%20Mill%20Run,%20PA&sa=X&ei=AcSiVNHGHJLOgwT7n4PADg&ved=0CI0BEPUXMA4&output=classic&dg=brw'>Directions</a>",
+            'marker-size': 'medium',
+            'marker-color': '#222222',
+            'marker-symbol': 'farm'
+        }
+    })
+    .addTo(_map);
+
+    L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [
+                _sevenSpringsLocation.lng,
+                _sevenSpringsLocation.lat
+            ]
+        },
+        properties: {
+            title: 'Seven Springs',
+            description: "Ski resort by winter, golf course by summer. There is a block of rooms for us here.<br/><br/><a target='_blank' href='http://www.7springs.com/lodging/hotel/'>Information</a>&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://maps.google.com/maps?um=1&ie=UTF-8&fb=1&gl=us&geocode=KTHneU33MzWIMbiaCnnTKxZu&daddr=Ski%20View%20Ave,%20Seven%20Springs,%20PA&sa=X&ei=useiVL78E8WZgwTl44DoAw&ved=0CJcBEPUXMA8&output=classic&dg=brw'>Directions</a>",
+            'marker-size': 'medium',
+            'marker-color': '#222222',
+            'marker-symbol': 'lodging'
+        }
+    })
+    .addTo(_map);
+}
+
+function fitBoundsToPointsOfInterest() {
+    var bounds = L.latLngBounds([_fallingwaterLocation, _sevenSpringsLocation]);
+    bounds = bounds.pad(0.5);
+
+    fitOptions = {
+        'animate':true,
+        'maxZoom':12,
+        'minZoom':12
+    }
+
+    _map.fitBounds(bounds, fitOptions);
+    _map.panTo(_centerOfMap);
 }
