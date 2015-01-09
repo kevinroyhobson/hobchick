ffd12dc03928d314c527a541a5e56041da864ade
Kevin Hobson <kevinroyhobson@gmail.com>
2015-01-08 21:35:02 -0500

    better handling of "who" page resizing

--- hobchick/hobchick/static/js/who.js
+++ hobchick/hobchick/static/js/who.js
@@ -4,8 +4,8 @@ function onResize() {
 
     var theCards = $('.baseball-card');
 
-    var windowHeight = $(window).height();
-    var cardHeight = windowHeight * .8;
+    var windowHeight = $(window).height() - $('.footer').height();
+    var cardHeight = windowHeight * .90;
     var cardWidth = cardHeight * 0.727379556;
 
     $('.flip-container').width(cardWidth);
@@ -13,13 +13,17 @@ function onResize() {
     $('.front').width(cardWidth);
     $('.back').width(cardWidth);
 
-
-
     $('.flip-container').height(cardHeight);
     $('.flipper').height(cardHeight);
     $('.front').height(cardHeight);
     $('.back').height(cardHeight);
 
+    var columnEmptySpaceWidth = Math.floor(($(window).width()/2) - cardWidth);
+    var pixelsToPushKevinRight = (5/6) * columnEmptySpaceWidth;
+    var pixelsToPushDanielleLeft = (1/6) * columnEmptySpaceWidth;
+
+    $('.kevin-card-holder').css('padding-left', pixelsToPushKevinRight + 'px');
+    $('.danielle-card-holder').css('padding-left', pixelsToPushDanielleLeft + 'px');
 }
 
 $(function() {
