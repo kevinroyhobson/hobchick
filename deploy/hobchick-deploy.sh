e1a8048dfaf7c0d2df5927e8faecb1a5e63a2159
Kevin Hobson <kevinroyhobson@gmail.com>
2015-01-15 22:55:02 -0500

    update deploy script to kill the non-minified js

--- deploy/hobchick-deploy.sh
+++ deploy/hobchick-deploy.sh
@@ -12,6 +12,8 @@ rm -r /var/www/static
 
 echo "Copying over new static files."
 cp -a -r hobchick/hobchick/static /var/www/static
+rm /var/www/static/js/*
+rm /var/www/static/js/mobile/*
 
 echo "Restarting web server."
 service apache2 restart
