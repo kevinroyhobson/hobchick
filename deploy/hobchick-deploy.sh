9fd7b05b974a957e3ebd518602821030f9e7dc95
Kevin Hobson <kevinroyhobson@gmail.com>
2021-01-29 23:02:45 +0000

    Made deploy script execution more smooth.

--- deploy/hobchick-deploy.sh
+++ deploy/hobchick-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Deploying hobchick.com..."
-
-apt-get install python3 python3-pip python3-django apache2 libapache2-mod-wsgi-py3
-pip3 install sendgrid
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3 python3-pip python3-django apache2 libapache2-mod-wsgi-py3
+sudo pip3 install sendgrid
 
 echo "Updating to the tip of master."
 cd ~/src/hobchick
@@ -12,14 +12,14 @@ git checkout master
 git pull
 
 echo "Killing old static files."
-rm -r /var/www/static
+sudo rm -r /var/www/static
 
 echo "Copying over new static files."
-cp -a -r hobchick/hobchick/static /var/www/static
-rm /var/www/static/js/*
-rm /var/www/static/js/mobile/*
+sudo cp -a -r hobchick/hobchick/static /var/www/static
+sudo rm /var/www/static/js/*
+sudo rm /var/www/static/js/mobile/*
 
 echo "Restarting web server."
-service apache2 restart
+sudo service apache2 restart
 
 echo "Deploy successful."
